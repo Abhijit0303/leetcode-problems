dce4caeb84e820986b26707dd4d5ec5cb8e4666c
Abhijit <kumarabhijeetpanda@rediffmail.com>
2026-01-17 22:29:19 +0530

    Done 1 problem on 17, Jan

---
+++ sliding-window/substring-with-concatination-of-all-words.py
@@ -0,0 +1,82 @@
+"""30. Substring with Concatenation of All Words
+Hard
+Topics
+premium lock icon
+Companies
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+Output: [0,9]
+
+Explanation:
+
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+
+Output: []
+
+Explanation:
+
+There is no concatenated substring.
+
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+
+Output: [6,9,12]
+
+Explanation:
+
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"].
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"].
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"].
+
+ """
+from collections import Counter
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        if not s or not words:
+            return []
+
+        word_length = len(words[0])
+        word_count = len(words)
+        total_length = word_length * word_count
+        word_map = Counter(words)
+        result = []
+
+        for i in range(len(s) - total_length + 1):
+            seen = {}
+            for j in range(word_count):
+                word_index = i + j * word_length
+                word = s[word_index:word_index + word_length]
+
+                if word not in word_map:
+                    break
+
+                seen[word] = seen.get(word, 0) + 1
+
+                if seen[word] > word_map[word]:
+                    break
+            else:
+                result.append(i)
+
+        return result
